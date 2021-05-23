7b450f419056a65862bf4bbbfb0852004474eb3d
remiplss <57995218+remiplss@users.noreply.github.com>
2021-05-23 20:15:35 +0200

    t

--- php/inscription.php
+++ php/inscription.php
@@ -11,19 +11,23 @@ $Email = isset($_POST["mail"]) ? $_POST["mail"] : "";
 $MotDePasse = isset($_POST["password"]) ? $_POST["password"] : "";
 
 if ($Pseudo == "") {
+
    echo "<script>alert(\"pseudo vide\")</script>";
    header('Location: compte.html');
-    }
+}
+    
 if ($Email == "") {
    
     echo "<script>alert(\"email vide\")</script>";
     header('Location: compte.html');
-    }
+}
+    
 if ($MotDePasse == "") {
     
     echo "<script>alert(\"mot de passe vide\")</script>";
     header('Location: compte.html');
 }
+
 elseif ($db_found) {
     $sql = "INSERT INTO utilisateur (ID,Type, Pseudo, Email, Password) VALUES ('','$Type','$Pseudo','$Email','$MotDePasse')";
    $result = mysqli_query($db_handle,$sql);
@@ -31,30 +35,13 @@ elseif ($db_found) {
    {
    die('impossible d’ajouter cet enregistrement : ' . mysqli_error($db_handle));
    }
-    $sql2 = "SELECT * FROM utilisateur";
-    echo "<h1>test</h1>";
-    $result2 = mysqli_query($db_handle, $sql2);
-    echo "<table border=\"1\">";
-    echo "<tr>";
-    echo "<th>" . "type" . "</th>";
-    echo "<th>" . "pseudo" . "</th>";
-    echo "<th>" . "mail" . "</th>";
-    echo "<th>" . "mdp" . "</th>";
-    echo "</tr>";
-
-    while ($data = mysqli_fetch_assoc($result2)) {
-        echo "<tr>";
-        echo "<td>" . $data['Type'] . "</td>";
-        echo "<td>" . $data['Pseudo'] . "</td>";
-        echo "<td>" . $data['Email'] . "</td>";
-        echo "<td>" . $data['Password'] . "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-    header('Location: Page d'acc.html');
+   
+    header('Location: Page d\'accueil.html');
+    
 } else {
     echo "<br>Database not found";
 }
+
 
 
 
